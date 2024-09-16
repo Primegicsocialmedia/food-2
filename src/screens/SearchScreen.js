add44b73c278724f5e465d53cbc3db7a81253634
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-16 16:41:00 +0200

    hooks

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,33 +2,20 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import { useState, useEffect } from "react";
-import yelp from "../api/yelp";
-
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-
-  const searchApi = async () => {
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: term,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      console.log(err);
-      setErrorMessage("Something went wrong");
-    }
-  };
+  const [searchApi, results, errorMessage] = useResults()
+ 
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
