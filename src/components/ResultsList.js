3f22dac3a1eefe6d272022ae660ba2c90997e3a0
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-16 17:48:51 +0200

    details

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,35 +1,34 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import React from 'react'
-import ResultsDetail from './ResultsDetail'
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React from "react";
+import ResultsDetail from "./ResultsDetail";
 
-const ResultsList = ({title, results}) => {
+const ResultsList = ({ title, results }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.titleStyle}>{title}</Text>
       <FlatList
+        showsHorizontalScrollIndicator={false}
         horizontal
         data={results}
         keyExtractor={() => results.id}
-        renderItem={({item}) => {
-            return <ResultsDetail result={item}/>
-        } }
+        renderItem={({ item }) => {
+          return <ResultsDetail result={item} />;
+        }}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    titleStyle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginLeft: 15,
-        marginBottom: 5,
+  titleStyle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 15,
+    marginBottom: 5,
+  },
+  container: {
+    marginBottom: 10,
+  },
+});
 
-    },
-    container: {
-        marginBottom: 10,
-    }
-})
-
-export default ResultsList
-
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React from "react";
 import SearchBar from "../components/SearchBar";
 import { useState } from "react";
@@ -7,28 +7,35 @@ import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [searchApi, results, errorMessage] = useResults()
+  const [searchApi, results, errorMessage] = useResults();
 
- const filterResultsByPrice = (price) => {
-    return results.filter(result => {
-        return result.price === price;
-    })
- }
- 
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
+  };
 
   return (
-    <View style={{marginLeft: 10}}>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
-      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective'/>
-      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier'/>
-      <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender'/>
-    </View>
+     
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice("$")}
+          title="Cost Effective"
+        />
+        <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice("$$$")}
+          title="Big Spender"
+        />
+      </ScrollView>
+    </>
   );
 };
 
